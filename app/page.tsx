49d7859e6930abb914d260326a7965717f8a80e5
Floatnarue <naroebordin@gmail.com>
2024-05-02 21:15:00 +0700

    fix font size

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
+import { store } from "@/libs/store";
 import Container from "./components/Container";
 import ListingCard from "./components/Listing/ListingCard";
 import { listing } from "@/libs/card";
-
+import { Provider } from "react-redux";
 export default function Home() {
   return (
-    <Container>
-      <div className="border-b-[1px] border-gray-400 rounded-full p-2">
-        <div className="flex justify-center items-center mb-4">
-          <div className="mt-4 font-bold text-lg">Pantip Realtime</div>
+    
+      <Container>
+        <div className="border-b-[1px] border-gray-400 rounded-full p-2">
+          <div className="flex justify-center items-center mb-4">
+            <div className="mt-4 font-bold text-lg">Pantip Realtime</div>
+          </div>
         </div>
-      </div>
 
-      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl-grid-cols-6 gap-8">
-        {listing.map((list) => {
-          return <ListingCard key={list.topic} data={list} />;
-        })}
-      </div>
-    </Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl-grid-cols-6 gap-8">
+          {listing.map((list) => {
+            return <ListingCard key={list.topic} data={list} />;
+          })}
+        </div>
+      </Container>
+    
   );
 }
--- libs/store.ts
+++ libs/store.ts
@@ -1,13 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import menuReducer from './userMenuSlice'
 
-export const makeStore = () => {
-  return configureStore({
-    reducer: {}
-  })
-}
+export const store = configureStore({
+  reducer: {
+    menu: menuReducer,
+  },
+});
 
-// Infer the type of makeStore
-export type AppStore = ReturnType<typeof makeStore>
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore['getState']>
-export type AppDispatch = AppStore['dispatch']
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
